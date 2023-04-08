0f9ad78ef61fe762b62e671f93c7515fa8d48809
Hao <haosun.uq@outlook.com>
2023-04-08 12:24:13 +1000

    u

--- src/components/Tailwind/Transition/Expand.tsx
+++ src/components/Tailwind/Transition/Expand.tsx
@@ -1,44 +1,84 @@
+import { useTick } from "@/lib/hooks/useTick";
 import gsap from "gsap";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
-export const Expand = (props: { open: boolean; children: any }) => {
+export const Expand = (props: {
+  open: boolean;
+  children: any;
+  visible?: boolean;
+  id?: number | string;
+}) => {
   const theRef = useRef(null);
   const heightRef = useRef(null);
   const expandTimeline = useRef(null);
   const shrinkTimeline = useRef(null);
-  const { open, children } = props;
+  const { open, children, visible, id } = props;
+  const [shouldClose, setShouldClose] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+  // const tick = useTick(1);
 
   useEffect(() => {
+    console.log((heightRef.current as any).offsetHeight);
     expandTimeline.current = gsap
       .timeline({ paused: true })
-      .to(theRef.current, {
-        height: (heightRef.current as any).offsetHeight,
-        duration: 0.3,
+      .fromTo(
+        theRef.current,
+        {
+          height: 0,
+        },
+        {
+          height: (heightRef.current as any).offsetHeight,
+          duration: 0.3,
+        }
+      )
+      .add(() => {
+        if (visible) setIsVisible(true);
       }) as any;
-
     shrinkTimeline.current = gsap
       .timeline({ paused: true })
-      .to(theRef.current, {
-        height: 0,
-        duration: 0.3,
-      }) as any;
-
+      .add(() => {
+        if (visible) setIsVisible(false);
+      })
+      .fromTo(
+        theRef.current,
+        { height: (heightRef.current as any).offsetHeight },
+        {
+          height: 0,
+          duration: 0.3,
+        }
+      ) as any;
+    if (!open) {
+      setShouldClose(true);
+      (shrinkTimeline.current as any).seek(0.5);
+    } else {
+      setShouldClose(false);
+    }
+    // setFirstOpen(true);
     return () => {
       (expandTimeline.current as any).kill();
       (shrinkTimeline.current as any).kill();
     };
-  }, [(heightRef.current as any)?.offsetHeight]);
+  }, [id]);
 
   useEffect(() => {
     if (open) {
       (expandTimeline.current as any).restart();
     } else {
-      (shrinkTimeline.current as any).restart();
+      if (shouldClose) {
+        setShouldClose(false);
+      } else {
+        (shrinkTimeline.current as any).restart();
+      }
     }
   }, [open]);
 
   return (
-    <div ref={theRef} className="overflow-hidden h-0">
+    <div
+      ref={theRef}
+      className={`relative ${
+        isVisible ? "overflow-visible" : "overflow-hidden"
+      }  h-0`}
+    >
       {children}
       <div
         ref={heightRef}
@@ -47,7 +87,7 @@ export const Expand = (props: { open: boolean; children: any }) => {
             ? (theRef.current as any).offsetWidth
             : undefined,
         }}
-        className="absolute -z-50 opacity-0"
+        className="pointer-event-none absolute left-0 top-0 -z-50 opacity-0"
       >
         {children}
       </div>
